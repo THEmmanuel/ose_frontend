f3a693bb214083f51c67eba59ab01966bde2ecaf
unknown <Emmanuel Ayodele>
2021-11-13 14:10:36 +0100

    Added new stuff... idk at this point.

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import style from './Landingpage.module.css'
-import landingBG from '../Assets/conifer-1138.png'
+import landingBG from '../Assets/conifer-1138.png';
+import SettlerBG from '../Assets/conifer-1138.png';
 
 const LandingPage = () => {
     return (
         <div className={style.LandingPage}>
             <section className={style.Introduction}>
-            <div className = {style.IntroductionContainer}>
+                <div className={style.IntroductionTextContainer}>
+                    <span className={style.IntroductionTitle}>
+                        Community-led
+                        Crosschain.
+                        DeFi Together.
+                    </span>
 
-            </div>
-                <span className={style.IntroductionTitle}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                </span>
+
+                    <span className={style.IntroductionText}>
+                        Sign up to join our Beta waitlist.
+                    </span>
+
+                    <input type="text" className={style.CTAInput} />
+
+                    <div className={style.CTAButtons}>
+                        <button className={style.CTAButton}>
+                            Learn more
+                        </button>
+
+                        <button className={style.CTAButton}>
+                            Learn more
+                        </button>
+                    </div>
+
+                </div>
 
                 <img
                     src={landingBG}
@@ -25,24 +45,38 @@ const LandingPage = () => {
                     <span>
                         75 seed investors
                     </span>
+
+                    <span>
+                        75 seed investors
+                    </span>
+
+                    <span>
+                        75 seed investors
+                    </span>
+
+                    <span>
+                        75 seed investors
+                    </span>
                 </div>
             </section>
 
             <section className={style.Settlers}>
-                <img src="" alt="" />
-                <div>
-                    <span>Title</span>
-                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo inventore optio similique! Ut laborum officiis doloribus culpa harum, distinctio illum totam numquam molestiae esse perspiciatis saepe tempora nulla autem deleniti.
-                    </span>
-                    <button>
-                        learn more
-                    </button>
+                <div className={style.SettlersContainer}>
+                    <img src={SettlerBG} alt="" className={style.SettlerBG} />
+                    <div className={style.SettlersTextContainer}>
+                        <span className={style.SettlerTitle}>Settlers</span>
+                        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo inventore optio similique! Ut laborum officiis doloribus culpa harum, distinctio illum totam numquam molestiae esse perspiciatis saepe tempora nulla autem deleniti.
+                        </span>
+                        <button className={style.CTAButton}>
+                            Learn more
+                        </button>
+                    </div>
                 </div>
             </section>
 
             <section className={style.Banner}>
-                <span>DeFi together.</span>
-                <span>Deposit capital, vote on investments, and earn rewards.</span>
+                <span className={style.BannerTitle}>DeFi together.</span>
+                <span className={style.BannerText}>Deposit capital, vote on investments, and earn rewards.</span>
             </section>
 
             <section className={style.FeatureCards}>
